1eea497225100451f11f8b2a9cd298d9c5e79532
chp320 <chp320@naver.com>
2024-03-16 00:53:12 +0900

    read text file function

---
+++ chap01/low_read.c
@@ -0,0 +1,11 @@
+//
+// Created by user on 2024-03-16.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "fcntl.h"
+#include "unistd.h"
+#define BUF_SIZE 100
+void error_handling(char* message);
+
